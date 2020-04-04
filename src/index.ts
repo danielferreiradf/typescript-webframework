47fdd48ee591c4637fd904f1bb8d363ed5285c98
danielferreiradf <danielferreiradk@gmail.com>
2020-04-04 18:02:21 +0100

    changed eventing on user class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import { User } from "./models/User";
 
 const user = new User({ name: "new record", age: 0 });
 
-user.save();
+// user.save();
+
+user.events.on("change", () => {
+  console.log("aaaa");
+});
+
+user.events.trigger("change");
