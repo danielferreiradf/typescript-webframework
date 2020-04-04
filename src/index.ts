79efbe7c98482e6ae2bf38b63be6f554c890c850
danielferreiradf <danielferreiradk@gmail.com>
2020-04-04 16:54:51 +0100

    added trigger event to user class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Daniel", age: 25 });
 
-user.set({ name: "new name", age: 99 });
+user.on("change", () => {
+  console.log("change1");
+});
+user.on("change", () => {
+  console.log("change2");
+});
+user.on("test", () => {
+  console.log("test");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("change");
+user.trigger("change2");
+
+console.log(user);
