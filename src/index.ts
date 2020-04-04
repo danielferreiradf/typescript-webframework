56d448468b30bc01cc6c841540f378ca4a472d2f
danielferreiradf <danielferreiradk@gmail.com>
2020-04-04 17:23:24 +0100

    added fetch method to user class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Daniel", age: 25 });
+const user = new User({ id: 1, name: "Daniel", age: 25 });
 
-user.on("change", () => {
-  console.log("change1");
-});
-user.on("change", () => {
-  console.log("change2");
-});
-user.on("test", () => {
-  console.log("test");
-});
-
-user.trigger("change");
-user.trigger("change2");
-
-console.log(user);
+user.fetch();
