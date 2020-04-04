31095bf0bbc725d3138ec885e24b05d076ac120f
danielferreiradf <danielferreiradk@gmail.com>
2020-04-04 16:43:42 +0100

    added events and on to user class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Daniel", age: 25 });
 
+user.set({ name: "new name", age: 99 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
